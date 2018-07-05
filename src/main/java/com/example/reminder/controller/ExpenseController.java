aa79503e944d71e4902e9d6d5ab9fb5cf7404399
Sergey Mikhaylov <sergeybonch@gmail.com>
2018-07-05 13:39:15 +0300

    Add missing controller method; Add style

--- src/main/java/com/example/reminder/controller/ExpenseController.java
+++ src/main/java/com/example/reminder/controller/ExpenseController.java
@@ -29,21 +29,23 @@ public class ExpenseController {
   @Autowired
   private CategoryService categoryService;
   
-  private static final List<String> YEARS = Lists.newArrayList("2018", "2019", "2020", "2021", "2022");
-  private static final ImmutableMap<Integer, String> MONTHS = ImmutableMap.<Integer, String>builder()
-	.put(Month.JANUARY.getValue(), "January")
-	.put(Month.FEBRUARY.getValue(), "February")
-	.put(Month.MARCH.getValue(), "March")
-	.put(Month.APRIL.getValue(), "April")
-	.put(Month.MAY.getValue(), "May")
-	.put(Month.JUNE.getValue(), "June")
-	.put(Month.JULY.getValue(), "July")
-	.put(Month.AUGUST.getValue(), "August")
-	.put(Month.SEPTEMBER.getValue(), "September")
-	.put(Month.OCTOBER.getValue(), "October")
-	.put(Month.NOVEMBER.getValue(), "November")
-	.put(Month.DECEMBER.getValue(), "December")
-	.build();
+  private static final List<String> YEARS =
+		  Lists.newArrayList("2018", "2019", "2020", "2021", "2022");
+  private static final ImmutableMap<Integer, String> MONTHS = 
+      ImmutableMap.<Integer, String>builder()
+		.put(Month.JANUARY.getValue(), "January")
+		.put(Month.FEBRUARY.getValue(), "February")
+		.put(Month.MARCH.getValue(), "March")
+		.put(Month.APRIL.getValue(), "April")
+		.put(Month.MAY.getValue(), "May")
+		.put(Month.JUNE.getValue(), "June")
+		.put(Month.JULY.getValue(), "July")
+		.put(Month.AUGUST.getValue(), "August")
+		.put(Month.SEPTEMBER.getValue(), "September")
+		.put(Month.OCTOBER.getValue(), "October")
+		.put(Month.NOVEMBER.getValue(), "November")
+		.put(Month.DECEMBER.getValue(), "December")
+		.build();
 
 
   @RequestMapping("/dates")
@@ -69,6 +71,15 @@ public class ExpenseController {
   public String newExpense(Model model) {
     List<Category> categories = categoryService.listAll();
     ExpenseForm form = new ExpenseForm(categories);
+    model.addAttribute("expenseForm", form);
+    return "expenseForm";
+  }
+  
+  @RequestMapping(value = "/expense/edit")
+  public String editExpense(@RequestParam Integer id, Model model) {
+    List<Category> categories = categoryService.listAll();
+    Expense expense = expenseService.getById(id);
+    ExpenseForm form = new ExpenseForm(expense, categories);
     model.addAttribute("expenseForm", form);
     return "expenseForm";
   }
